d597a1f7b6d7e36c24d81de872cbcd0c67f95ba1
Jamie Robson <jro@umbraco.dk>
2024-03-15 19:43:57 +0100

    working out why we just loop instead of solving cells

--- src/sudoku/techniques/closed_groups.rs
+++ src/sudoku/techniques/closed_groups.rs
@@ -1,5 +1,5 @@
 use crate::{pretty::aliases::*, sudoku::core::{cell_grid::CellReference, consts::PUZZLE_TOTAL_CELL_COUNT}};
-use std::{cell, collections::{hash_map, HashMap}};
+use std::{cell, collections::{hash_map, HashMap}, ops::Add};
 
 use crate::sudoku::core::{cell::Cell, game::Game, validatable_units::CellGroup};
 
@@ -17,7 +17,6 @@ pub fn eliminate_candidates_from_closed_groups(game: &mut Game) {
         let any_eliminated = try_eliminate_closed_candidate_groups(game);
 
         if !any_eliminated {
-            //todo: Why do we abandon after the first iteration?
             println!("Finished eliminating candidates from closed groups after {} iterations", i);
             break;
         }
@@ -50,11 +49,11 @@ fn try_naive_eliminate_options_from_groups(cell_group_vector: &mut Vector<CellGr
     
     for cell_group in cell_group_vector {
         
-        let mut dictionary : HashMap<String, Vector<CellReference>>= HashMap::new();
+        let mut dictionary : HashMap<String, usize>= HashMap::new();
         
         let all_keys: Vector<_> = cell_group.cells.iter().filter(|cell| cell.borrow().value.is_none()).map(|cell| key_from_potential_values(&cell)).collect();
         for key in all_keys.clone() {
-            dictionary.insert(key, vec![]);
+            dictionary.insert(key, 0);
         }
 
         for cell_reference in &cell_group.cells {
@@ -64,26 +63,33 @@ fn try_naive_eliminate_options_from_groups(cell_group_vector: &mut Vector<CellGr
 
             all_keys.iter()
             .filter(|key| cell_reference.borrow().potentially_valid_values.iter().all(|c| key.contains(&c.to_string())))
-            .for_each(|key| dictionary.get_mut(key).expect("we did just check that the key exists").push(cell_reference.clone()));
+            .for_each(|key| {
+                if let Some(count) = dictionary.get(key) {
+                    dictionary.insert(key.clone(), *count + 1);
+                }
+            });
         }
 
         for dictionary_entry in dictionary {
            
             let values_to_discount = values_from_key(&dictionary_entry.0);
             
-            if values_to_discount.len() != dictionary_entry.1.len() 
+            if values_to_discount.len() != dictionary_entry.1
             {
                 continue;
             }
 
-            if cell_group.cells.len() == dictionary_entry.1.len() {
+            if cell_group.cells.len() == dictionary_entry.1 {
                 continue;
             }
 
             println!("cycle identified for key {}", &dictionary_entry.0);
-
+ 
             for cell in cell_group.cells.iter() {
 
+                // if cell is one of the cells in the group then we shouldn't alter its candidates
+                // Only if a cell is actually mutated in some way do we want to report that something happened
+                // and that it is work continuing another iteration
                 if cell.borrow().potentially_valid_values.iter().all(|c| dictionary_entry.0.contains(&c.to_string())) {
                     continue;
                 }
@@ -173,6 +179,20 @@ mod tests {
     #[test]
     fn try_solve_a_puzzle_that_could_not_be_immediately_implicitely_solved (){
         let test_case = "328975641..13..572.7....839..27....3..7..32.68..6.2..74.9..73687..8..124286134795";
+        let mut game = Serializer::new().new_game(test_case).expect("test data is valid");
+        
+        let initial_cell_count = game.count_cells_with_value();
+        draw_all_rows(&game.rows);
+
+        eliminate_candidates_from_closed_groups(&mut game);
+        
+        draw_all_rows(&game.rows);
+        println!("before: {}, after: {}", initial_cell_count, game.count_cells_with_value());
+    }
+    
+    #[test]
+    fn try_solve_a_puzzle_with_closed_group (){
+        let test_case = ".8..9..3..3.....699.2.63158.2.8.459.8519.7.463946.587.563.4.9872......15.1..5..2.";
         let mut game = Serializer::new().new_game(test_case).expect("test data is valid");
         
         let initial_cell_count = game.count_cells_with_value();
