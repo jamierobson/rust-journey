bad0f8d50d77748d9f108796f7f4502876f5db9e
Jamie Robson <jro@umbraco.dk>
2024-03-19 20:25:14 +0100

    Beginning to implement the brute force idea. It's shaky for now, needs a result type (all of the methods do), but getting there slowly

--- src/pretty/aliases.rs
+++ src/pretty/aliases.rs
@@ -3,10 +3,15 @@ pub type StringSlice<'a> = &'a str;
 
 pub trait Iteratable<T> {
     fn iterate(&self) -> std::slice::Iter<'_, T>;
+    fn into_iterator(&self) -> std::slice::Iter<'_, T>;
 }
 
 impl<T>Iteratable<T> for [T] {
     fn iterate(&self) -> std::slice::Iter<'_, T> {
         self.iter()
+    }
+
+    fn into_iterator(&self) -> std::slice::Iter<'_, T>{
+        self.into_iter()
     }
 }
